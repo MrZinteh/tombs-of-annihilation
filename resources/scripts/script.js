4d93bf41effc386c012cc37a95d5f4d237cd8069
hamerl64 <hamerl64@imec.be>
2021-07-09 16:57:37 +0200

    Added experimental fadeout

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -17,29 +17,33 @@ function fadeIn(el, time) {
     tick();
 }
 
-// function fadeOut(el, time) {
-//     el.style.opacity = 100;
+function fadeOut(el, time) {
+    el.style.opacity = 1;
   
-//     var last = +new Date();
-//     var tick = function() {
-//       el.style.opacity = +el.style.opacity - (new Date() - last) / time;
-//       last = +new Date();
+    var last = +new Date();
+    var tick = function() {
+      el.style.opacity = +el.style.opacity - (new Date() - last) / time;
+      console.log(+el.style.opacity);
+      last = +new Date();
   
-//       if (+el.style.opacity > 0) {
-//         (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
-//       }
-//     };
+      if (+el.style.opacity > 0) {
+        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+      }
+    };
   
-//     tick();
-// }
+    tick();
+}
+
+let toggle = false;
 
 playerMapElement.addEventListener('click', function (event) {
-    if(playerMapElement.style.opacity === 0) {
-        fadeIn(playerMapElement, 3000);
+    if(toggle === false) {
+        fadeOut(playerMapElement, 3000);
+        toggle = true;
     }
-    // else {
-    //     fadeOut(playerMapElement, 3000);
-    // }
+    else {
+        fadeOut(playerMapElement, 3000);
+    }
 });
 
 // playerMapElement.addEventListener('click', function (event) {
