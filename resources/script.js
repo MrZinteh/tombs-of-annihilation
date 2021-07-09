da5c4a1549b3fca005ca5d48859d65f13eb76d43
hamerl64 <hamerl64@imec.be>
2021-07-09 14:16:13 +0200

    Added debugging alert to script

--- resources/script.js
+++ resources/script.js
@@ -1,6 +1,7 @@
 const mapElement = document.getElementById('chult-map');
 
 mapElement.addEventListener('click', function (event) {
+    alert(mapElement.src);
     if(mapElement.src === 'https://media-waterdeep.cursecdn.com/attachments/2/858/0302.jpg') {
         mapElement.src = "https://media-waterdeep.cursecdn.com/attachments/2/937/ph01.jpg";
     }
