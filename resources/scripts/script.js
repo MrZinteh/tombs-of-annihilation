43d178a7048487f3604a58eb889c92b80768536b
hamerl64 <hamerl64@imec.be>
2021-07-09 16:34:15 +0200

    Added fadein

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,18 +1,60 @@
 const dmMapElement = document.getElementById('chult-map-dm');
 const playerMapElement = document.getElementById('chult-map-player');
 
-dmMapElement.addEventListener('click', function (event) {
-    alert("clicked!");
-    if(dmMapElement.style.opacity === 0) {
-        for(let i=0;i<=100;i++) {
-            dmMapElement.style.opacity = i;
-            playerMapElement.style.opacity = 100-i;
-        }
+function fadeIn(el, time) {
+    el.style.opacity = 0;
+  
+    var last = +new Date();
+    var tick = function() {
+      el.style.opacity = +el.style.opacity + (new Date() - last) / time;
+      last = +new Date();
+  
+      if (+el.style.opacity < 1) {
+        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+      }
+    };
+  
+    tick();
+}
+
+// function fadeOut(el, time) {
+//     el.style.opacity = 100;
+  
+//     var last = +new Date();
+//     var tick = function() {
+//       el.style.opacity = +el.style.opacity - (new Date() - last) / time;
+//       last = +new Date();
+  
+//       if (+el.style.opacity > 0) {
+//         (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+//       }
+//     };
+  
+//     tick();
+// }
+
+playerMapElement.addEventListener('click', function (event) {
+    if(playerMapElement.style.opacity === 0) {
+        fadeIn(playerMapElement, 3000);
     }
-    else {
-        for(let i=0;i<=100;i++) {
-            dmMapElement.style.opacity = 100-i;
-            playerMapElement.style.opacity = i;
-        }
-    }
-  });
+    // else {
+    //     fadeOut(playerMapElement, 3000);
+    // }
+});
+
+// playerMapElement.addEventListener('click', function (event) {
+//     if(dmMapElement.style.opacity === 0) {
+//         for(let i=0;i<=100;i++) {
+//             dmMapElement.style.opacity = i;
+//             playerMapElement.style.opacity = 100-i;
+//         }
+//     }
+//     else {
+//         for(let i=0;i<=100;i++) {
+//             dmMapElement.style.opacity = 100-i;
+//             playerMapElement.style.opacity = i;
+//         }
+//     }
+//   });
+
+  
