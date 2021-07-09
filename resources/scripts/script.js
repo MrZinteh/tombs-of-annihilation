b563585a630f560f0398b04ffbbb7415c26da654
hamerl64 <hamerl64@imec.be>
2021-07-09 16:01:50 +0200

    Added experimental transition JS

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,10 +1,18 @@
-const mapElement = document.getElementById('chult-map');
+const dmMapElement = document.getElementById('chult-map-dm');
+const playerMapElement = document.getElementById('chult-map-player');
 
-mapElement.addEventListener('click', function (event) {
-    if(mapElement.src === 'https://media-waterdeep.cursecdn.com/attachments/2/858/0302.jpg') {
-        mapElement.src = "https://media-waterdeep.cursecdn.com/attachments/2/937/ph01.jpg";
+dmMapElement.addEventListener('click', function (event) {
+    alert("clicked!");
+    if(dmMapElement.style.opacity === 0) {
+        for(let i=0;i<=100;i++) {
+            dmMapElement.style.opacity = i;
+            playerMapElement.style.opacity = 100-i;
+        }
     }
     else {
-        mapElement.src = "https://media-waterdeep.cursecdn.com/attachments/2/858/0302.jpg";
+        for(let i=0;i<=100;i++) {
+            dmMapElement.style.opacity = 100-i;
+            playerMapElement.style.opacity = i;
+        }
     }
   });
